7221829014bd1d0a3c394118e6d4c745ad11620b
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-07-13 18:26:31 +0200

    Add info part.

---
+++ src/components/Info.jsx
@@ -0,0 +1,13 @@
+export default function Info() {
+  return (
+    <div className="info-container">
+      <h1>Ehsan Mehrali</h1>
+      <span>Full Stack Developer</span>
+      <a>ehsan.com</a>
+      <div className="btn-container">
+        <button className="btn">Email</button>
+        <button className="btn btn-linkedin">LinkedIn</button>
+      </div>
+    </div>
+  );
+}
